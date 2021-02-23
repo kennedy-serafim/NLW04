0923037a8f03016c45a67da653621cce4804430e
Kennedy Serafim <serafimkennedy@gmail.com>
2021-02-23 21:14:38 +0200

    Create users migration

---
+++ api/src/database/migrations/1614106228917-CreateUsers.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUsers1614106228917 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "users",
+        columns: [
+          {
+            name: "id",
+            isPrimary: true,
+            type: "uuid",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "email",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("users");
+  }
+}
